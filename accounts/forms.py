2dc015191fc8e894966b2a24eda824f0104e53b1
Sushant7-codes <sushantpahari06@gmail.com>
2025-09-03 20:36:40 +0545

    after changing the designs of login and register form

--- accounts/forms.py
+++ accounts/forms.py
@@ -93,8 +93,19 @@ class RetailAdminRegisterForm(forms.Form):
         ),
     )
     profile_pic = forms.ImageField(
-        widget=forms.ClearableFileInput(attrs={"class": "input-file"}), required=False
-    )
+    required=False,
+    widget=forms.ClearableFileInput(
+        attrs={
+            "class": "block w-full text-sm text-gray-500 \
+                      file:mr-4 file:py-2 file:px-4 \
+                      file:rounded-full file:border-0 \
+                      file:text-sm file:font-semibold \
+                      file:bg-indigo-600 file:text-white \
+                      hover:file:bg-indigo-700 cursor-pointer"
+        }
+    ),
+)
+
 
     def clean(self):
         print(self.cleaned_data["password1"], self.cleaned_data["password2"])
