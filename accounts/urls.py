276bfc1d80a61e79e9fec839386e09f00cb6a1a4
Sushant7-codes <sushantpahari06@gmail.com>
2025-09-03 20:13:54 +0545

    login and register basics, before changing design

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ app_name = "accounts"
 urlpatterns = [
     path("register/", views.retail_admin_register, name="retail_admin_register"),
     path("login/", views.retail_admin_login, name="retail_admin_login"),
+    path("logout/", views.logout_view, name="logout_view"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from .forms import RetailAdminRegisterForm, RetailAdminLoginForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
 
 def retail_admin_register(request):
     if request.method == "POST":
-        form = RetailAdminRegisterForm(request.POST)
+        form = RetailAdminRegisterForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return redirect("accounts:retail_admin_login")
@@ -17,7 +19,26 @@ def retail_admin_register(request):
 
 
 def retail_admin_login(request):
-    form = RetailAdminLoginForm()
+    
+    if request.method == "POST":
+        username=request.POST.get("username")
+        password=request.POST.get("password")
+    
+        user = authenticate(request, username=username, password=password)
 
+        if user is not None:
+            login(request, user)
+            return redirect("app:dashboard")
+            
+        messages.error(request, "Invalid username or password")    
+        return redirect("accounts:retail_admin_login")
+        
+    form = RetailAdminLoginForm()
+    
     context = {"form": form}
     return render(request, "accounts/login.html", context)
+
+def logout_view(request):
+    logout(request)
+    return redirect("accounts:retail_admin_login")
+
