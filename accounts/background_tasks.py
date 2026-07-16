fc08913c806c7bceb16d12ce86078f7f70dd143e
Sushant7-codes <sushantpahari06@gmail.com>
2026-07-16 17:02:21 +0545

    check error for SMTP

--- accounts/background_tasks.py
+++ accounts/background_tasks.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.core.mail import EmailMessage
 
 # from django.core.mail import send_mail
 
@@ -29,6 +30,32 @@ from django.conf import settings
 
 #     send_mail(subject, message, settings.EMAIL_HOST_USER, [email])
 
+
 def send_otp(email, new_otp, purpose="forgot"):
-    print("OTP WOULD BE:", new_otp)
-    return
+
+    if purpose == "register":
+        subject = "Verify Your Email"
+        message = f"Your OTP is {new_otp}"
+    else:
+        subject = "Password Reset OTP"
+        message = f"Your OTP is {new_otp}"
+
+    print("EMAIL:", settings.EMAIL_HOST_USER)
+
+    try:
+        mail = EmailMessage(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [email],
+        )
+
+        mail.send(fail_silently=False)
+
+        print("EMAIL SENT SUCCESSFULLY")
+
+    except Exception as e:
+        print("EMAIL ERROR:")
+        print(type(e))
+        print(e)
+        raise
